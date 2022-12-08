e5b709e891676a6b47524e81ac5891f3f7b3d33a
Oliver Macpherson <oliver.macpherson@kcl.ac.uk>
2022-12-08 13:32:51 +0000

    Change to ModelForm

--- things/forms.py
+++ things/forms.py
@@ -4,7 +4,13 @@ from django import forms
 from .models import Thing
 from django.core.validators import MinValueValidator, MaxValueValidator
 
-class ThingForm(forms.Form):
-    name = forms.CharField(label="Name", max_length=35)
-    description = forms.CharField(label="Description", max_length=120, required=False, widget=forms.Textarea())
-    quantity = forms.CharField(label="Quantity", validators=[MinValueValidator(0), MaxValueValidator(50)], widget=forms.NumberInput())
+class ThingForm(forms.ModelForm):
+    class Meta:
+        model = Thing
+        fields = ['name', 'description', 'quantity']
+        widgets = {'description': forms.Textarea()}
+
+
+    # name = forms.CharField(label="Name", max_length=35)
+    # description = forms.CharField(label="Description", max_length=120, required=False, widget=forms.Textarea())
+    # quantity = forms.CharField(label="Quantity", validators=[MinValueValidator(0), MaxValueValidator(50)], widget=forms.NumberInput())
