17515610b9cd772b9764486542c015699c12e3e1
Oliver Macpherson <oliver.macpherson@kcl.ac.uk>
2022-12-08 13:27:43 +0000

    Complete solution

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,10 @@
 """Forms of the project."""
 
-# Create your forms here.
+from django import forms
+from .models import Thing
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+class ThingForm(forms.Form):
+    name = forms.CharField(label="Name", max_length=35)
+    description = forms.CharField(label="Description", max_length=120, required=False, widget=forms.Textarea())
+    quantity = forms.CharField(label="Quantity", validators=[MinValueValidator(0), MaxValueValidator(50)], widget=forms.NumberInput())
--- things/views.py
+++ things/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from .forms import ThingForm
+from .models import Thing
 
 def home(request):
+    form = ThingForm()
+    if form.is_valid():
+        name = form.cleaned_data.get('name')
+        desc = form.cleaned_data.get('description')
+        quantity = form.cleaned_data.get('quantity')
+        thing = Thing.objects.create(name=name, description=desc, quantity=quantity)
     return render(request, 'home.html', {'form': form})
